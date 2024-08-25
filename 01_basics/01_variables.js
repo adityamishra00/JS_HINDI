0c0002420d90e608ad8336b95e9c96fc8a4fd06f
Aditya Mishra <116947307+adityamishra00@users.noreply.github.com>
2024-08-25 07:23:57 +0000

    variales

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountID=14433
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountCity]) 
