de46a9469b2b750a428297048cb3f79350f2d6f9
Aditya Mishra <116947307+adityamishra00@users.noreply.github.com>
2024-08-25 07:24:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict";//treat all js code to newer version
+//alert(3+3) we are using nodejs,not browser
+//number=> 2to power53
+//bigint
+//string=>""
+//boolean =>true/false
+//null=>standalone value
+//undefined=>
+  //symbol=> unique
+
+//object
+
+console.log(typeof"aditya");
