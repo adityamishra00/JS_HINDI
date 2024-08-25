d7d260ce1ff40452df441607cbadf9a690c4596e
Aditya Mishra <116947307+adityamishra00@users.noreply.github.com>
2024-08-25 08:38:13 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score ="33abc"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+//op=>string
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//op=>number
+//console.log(valueInNumber);
+//if value have number only then it show value otherwise it show => NaN
+
+//"33"=> 33
+//"33abc"=> NaN
+// true => 1; false => 0
+
+  let isLoggedIn=1
+  
+  let booleanIsLoggedIn = Boolean(isLoggedIn)
+  //console.log(booleanIsLoggedIn);
+
+  //1=>true; 0=>false
+  //""=> false
+  //"hitesh"=> true
+
+  let someNumber=33
+
+  let stringNumber =String(someNumber)
+  console.log(stringNumber);
+  console.log(typeof stringNumber);
