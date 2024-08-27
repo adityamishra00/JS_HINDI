66a847098bc784f8a5282796a38a6e5a42a8a1ed
Aditya Mishra <116947307+adityamishra00@users.noreply.github.com>
2024-08-27 14:28:08 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,5 +25,26 @@ let valueInNumber=Number(score)
   let someNumber=33
 
   let stringNumber =String(someNumber)
-  console.log(stringNumber);
-  console.log(typeof stringNumber);
+  //console.log(stringNumber);
+  //console.log(typeof stringNumber);
+
+  // ********** Operations ***********
+
+  let value =34;
+  let negValue =-value;
+  //console.log(negValue);
+
+  //console.log(2+2);
+  //console.log("1"+2);
+  //console.log(1+"2");
+
+  //console.log(+true);
+  //console.log(+"");
+
+  let num1,num2,num3;
+  num1=num2=num3=2+2
+  //console.log(num1);
+
+  let gameCounter =100
+  ++gameCounter;
+  console.log(gameCounter);
