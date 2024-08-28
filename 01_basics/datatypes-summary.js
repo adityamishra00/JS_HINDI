e7723b6493c868f0d7552cf4526547569917f2f6
Aditya Mishra <116947307+adityamishra00@users.noreply.github.com>
2024-08-28 11:13:25 +0000

    datatypes summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -22,11 +22,11 @@ let myOj={
   age:22,
 }
 const myFunction=function(){
-  console.log("Hello World");
+  //console.log("Hello World");
 }
 myFunction();
 
-console.log(typeof myFunction);
+//console.log(typeof myFunction);
 
 
 //const igNumer =5547656754675475n
@@ -34,3 +34,25 @@ console.log(typeof myFunction);
 //Reference (Non primitive)
 
 //Array ,Ojects,Functions
+
+//************************
+
+//stack (Primitive) , Heap(Non-Primitive)
+
+let myYoutubeName="hiteshchoudharydotcom"
+
+let anotherName=myYoutubeName
+anotherName="aditya"
+
+//console.log(myYoutubeName);
+//console.log(anotherName);
+let userOne ={
+  email:"user@gmail.com",
+  upi:"user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email="hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
