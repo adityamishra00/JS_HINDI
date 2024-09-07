6bfe6b652bb4e664010a6f7d480e904afb38c819
Aditya Mishra <116947307+adityamishra00@users.noreply.github.com>
2024-09-07 14:04:49 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,50 @@
+// let a=10
+// const b=20
+// var c=30
+var c = 300;
+if(true){
+  let a=10
+  const b=20
+  var c=30
+}
+var c= 400
+
+//console.log(a); -> a is not defined
+//console.log(b); -> b is not defined
+//console.log(c); //op-30
+
+// ***** nested function 
+
+function one(){
+  const username ="aditya"
+
+  function two(){
+    const website = "Youtube"
+    //console.log(username);
+  }
+  //console.log(website);
+  two()
+}
+
+one()
+
+if(true){
+  const username ="hitesh"
+  if(username === "hitesh"){
+    //const website ="youtube"
+    //console.log(username + website); - aditya youtube
+  }
+ // console.log(website); -> website is not defined
+}
+//console.log(username); -> user name is not defined
+
+//************ interesting *********
+//console.log(addone(5)); -> we can declear first before using the function if we are not storing function in variable
+function addone(num){
+  return num+1;
+}
+//addTwo(5); -> it give error because function are stored in variable
+const addTwo = function(num){
+  return num+2;
+}
+console.log(addTwo(8)); // correct
