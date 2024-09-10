45a75ed055b758e0142a3f0c462bbdeed3636cb8
Aditya Mishra <116947307+adityamishra00@users.noreply.github.com>
2024-09-10 17:32:06 +0000

    forEach

---
+++ 05_iterations/five.js
@@ -0,0 +1,47 @@
+// for each
+const coading= ["js","ruby","java","c+++"]
+// coading.forEach(function fun(val){
+//   console.log(val);
+// })
+// coading.forEach(function (item){
+//   console.log(item);
+// })
+// coading.forEach( (item)=>{
+//   console.log(item);
+// })
+
+// all give the same output
+
+function printme(item){
+  //console.log(item);
+}
+
+coading.forEach(printme)//only give referance
+
+coading.forEach((item,index,arr)=>{
+  //console.log(item,index,arr);
+})
+
+const mycoding=[
+  {
+    languageNmae:"javascript",
+    langauageFileName:"js"
+  },
+  {
+    languageNmae:"java",
+    langauageFileName:"java"
+  },
+  {
+    languageNmae:"python",
+    langauageFileName:"phythom"
+  },
+  {
+    languageNmae:"cpp",
+    langauageFileName:"cpp"
+  },
+  
+]
+
+mycoding.forEach((item)=>{
+  console.log(item.langauageFileName)
+})
