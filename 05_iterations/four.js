5a45e8779d0c59e349a255d458431348f61e1c26
Aditya Mishra <116947307+adityamishra00@users.noreply.github.com>
2024-09-10 16:58:53 +0000

    for in

---
+++ 05_iterations/four.js
@@ -0,0 +1,27 @@
+const myObject={
+  js:'javascript',
+  cpp:'c++',
+  rb:"rubyt",
+  swift:"swift by apple"
+}
+
+for (const key in myObject) {
+  console.log(key);// this give key
+//  console.log(myObject[key]);//it give object
+}
+
+const programmming=["js","rb","py","java"]
+for (const key in programmming) {
+//  console.log(key);
+//  console.log(programmming[key])//it give value
+}
+
+const map=new Map()
+map.set('IN',"India")
+map.set('USA',"United States Of America")
+map.set('Fr',"France")
+
+
+for(const key in map){
+  //console.log(key);// this canmt give you the value because map is not iteratable
+}
