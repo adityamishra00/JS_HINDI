4435051a649b00541de71943e63f51462f2d99ce
Aditya Mishra <116947307+adityamishra00@users.noreply.github.com>
2024-09-10 14:12:07 +0000

    do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+// let index=0;
+// while(index<=10){
+//   console.log(`value : ${index+index}`)
+//   index++;
+// }
+
+
+let score=11;
+do{
+console.log(`score is ${score}`);
+score++;
+}while(score<=10);
+
+//op-11
