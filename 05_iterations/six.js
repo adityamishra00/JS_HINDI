60ac4ffc1265f75ce1296b3e5a577fad882c4739
Aditya Mishra <116947307+adityamishra00@users.noreply.github.com>
2024-09-11 17:55:25 +0000

    filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,27 @@
+const coading= ["js","ruby","java","c+++"]
+
+// const values= coading.forEach((item)=>{
+//   //console.log(item);
+//   return item;
+// })
+
+// console.log(values);
+
+ const myNums =[1,2,3,4,5,6,7,8]
+// const newNums = myNums.filter((num)=> num >4)
+// use to filter 
+
+//if we use {} then use return 
+//console.log(newNums);
+
+// const newnums=[]
+
+// myNums.forEach((num)=>{
+//   if(num>4){
+//     newnums.push(num)
+//   }
+// })
+
+// console.log(newnums);
+
+
