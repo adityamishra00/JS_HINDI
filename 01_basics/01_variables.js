98a55106420ca127835515470ab40c3b8b9b1e9b
Aditya Mishra <116947307+adityamishra00@users.noreply.github.com>
2024-10-08 18:02:41 +0000

    new commit

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,5 +2,12 @@ const accountID=14433
 let accountEmail="hitesh@google.com"
 var accountPassword="12345"
 accountCity="jaipur"
-console.log(accountID);
-console.table([accountID,accountEmail,accountPassword,accountCity]) 
+// console.log(accountID);
+// console.table([accountID,accountEmail,accountPassword,accountCity]) 
+
+//let ram=12345;
+{
+let ram=125;
+ }
+ 
+ console.log(ram);
