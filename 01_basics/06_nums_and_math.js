72aedb20035dc571bcc2521ed059b02a77a58876
Aditya Mishra <116947307+adityamishra00@users.noreply.github.com>
2024-10-08 18:05:56 +0000

    recheck

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -10,22 +10,22 @@ const otherNumber =123.8966
 //console.log(otherNumber.toPrecision(4));
 
 const hundreds =10000000
-////console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toLocaleString('en-IN'));
 
 // ******** MATH ****************
 
-//console.log(Math);
-//console.log(Math.abs(-4))
-//MAth.round(4.9)
-//Math.ceil(4.2)
-// Math.floor(4.9)
-//Math.min(4,3,6,8);
-//Math.max(4,3,6,8);
+console.log(Math);
+console.log(Math.abs(-4))
+MAth.round(4.9)
+Math.ceil(4.2)
+Math.floor(4.9)
+Math.min(4,3,6,8);
+Math.max(4,3,6,8);
 
-console.log(Math.random());
-console.log((Math.random()*10)+1);
-console.log(Math.floor(Math.random()*10)+1);
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
 
-const min=10;
-const max=20;
-console.log(Math.floor(Math.random()*(max-min))+min);
+// const min=10;
+// const max=20;
+// console.log(Math.floor(Math.random()*(max-min))+min);
