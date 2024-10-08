57fb9c7fc31acc63978bc34618e1354637b7c0f1
Aditya Mishra <116947307+adityamishra00@users.noreply.github.com>
2024-10-08 18:04:57 +0000

    string commit

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -4,24 +4,24 @@ const  repoCount=50
 //console.log(name+ repoCount+" value");
 //console.log(`Hello my name is  ${name} and my repo count is ${repoCount}`);
 const gameName=new String ('aditya')
-//console.log(gameName[0]);
-//cosnole.log(gameName.__promo__);
-//console.log(gameName.length);
-//console.log(gameName.toUpperCase());
-//console.log(gameName.charAt(2));
-//console.log(gameName.indexOf('t'));
+// console.log(gameName[0]);
+// console.log(gameName.__promo__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
 const newString = gameName.substring(0,4)
-//console.log(newString);
+console.log(newString);
 
-const anotherString= gameName.slice(-7,2)
-//console.log(anotherString); us eto print from reverse
+ const anotherString= gameName.slice(-7,2)
+console.log(anotherString); //us eto print from reverse
 
-const newStringone= "   hitesh   ";
-//console.log(newStringone);
-//console.log(newStringone.trim())
-//remove the out space
+// const newStringone= "   hitesh   ";
+// //console.log(newStringone);
+// //console.log(newStringone.trim())
+// //remove the out space
 
 const url= "https://hitesh.com/hitesh%20choudhary"
 
-console.log(url.replace('%20','-'))
-console.log(url.includes('sundar'))
+console.log(url.replace('%20','hi'))
+console.log(url.includes('https'))
