4375365384972f9145019d17234148136f070c94
Aditya Mishra <116947307+adityamishra00@users.noreply.github.com>
2024-10-09 07:05:08 +0000

    checked

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -1,6 +1,6 @@
  const arr=[1,2,3]
  const myNum= arr.reduce(function(acc,currval){
-  //console.log(`acc: ${acc} and currval: ${currval}`)
+  console.log(`acc: ${acc} and currval: ${currval}`)
 return acc+currval;
  },0)// 0 is using to start from 
 
@@ -23,8 +23,8 @@ return acc+currval;
   
  ]
 
- const ans=shoppingCard.reduce(function(acc,item){
-  return acc+item.price
- },0)
- console.log(ans);
- console.log(ans);
+//  const ans=shoppingCard.reduce(function(acc,item){
+//   return acc+item.price
+//  },0)
+//  console.log(ans);
+//  console.log(ans);
