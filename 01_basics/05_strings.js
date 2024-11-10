ea808c4570c7241b649cb9332a2fc805ea24ad66
Aditya Mishra <116947307+adityamishra00@users.noreply.github.com>
2024-11-10 06:25:12 +0000

    check 2

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,7 +5,7 @@ const  repoCount=50
 //console.log(`Hello my name is  ${name} and my repo count is ${repoCount}`);
 const gameName=new String ('aditya')
 // console.log(gameName[0]);
-// console.log(gameName.__promo__);
+ console.log(gameName.__promo__);
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
 // console.log(gameName.charAt(2));
